c27f7397f208d04ceefe2dae7d45bf6eb41ba262
Nguyen Van Manh <manhnvhanu@gmail.com>
2016-09-29 09:41:16 +0700

    Impelement reset password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation user
     @user = user
-    mail to: user.email, subject: (t "user_mailer.account_activation.subject")
+    mail to: user.email, subject: t "user_mailer.account_activation.subject"
   end
 
-  def password_reset
-    @greeting = t "user_mailer.password_reset.hello"
-
-    mail to: "to@example.org"
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t "user_mailer.password_reset.subject"
   end
 end
